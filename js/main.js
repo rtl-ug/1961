20749304c97b7a74569a8aeb5b52208e068e606c
kanyeceejaya <kakbr800@gmail.com>
2021-01-24 09:19:29 +0300

    fix menu and articles

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@
   
         // Using jQuery's animate() method to add smooth page scroll
         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('#navbarResponsive').collapse('toggle');
         $('html, body').animate({
           scrollTop: $(hash).offset().top
         }, 800, function(){
